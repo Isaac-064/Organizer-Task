ed47d71249a9113be5e671c08f690dd75a632312
Isaac Bautista <146866046+Isaac-064@users.noreply.github.com>
2024-04-17 17:55:03 -0600

    Creacion del Login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAxKSD_stAKuuR8mL6MQ9eEXSNzIMS2VbM",
+    authDomain: "organizer-task-df58e.firebaseapp.com",
+    projectId: "organizer-task-df58e",
+    storageBucket: "organizer-task-df58e.appspot.com",
+    messagingSenderId: "761819540181",
+    appId: "1:761819540181:web:c0b184a0ebc53111ab505c"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAxKSD_stAKuuR8mL6MQ9eEXSNzIMS2VbM",
+    authDomain: "organizer-task-df58e.firebaseapp.com",
+    projectId: "organizer-task-df58e",
+    storageBucket: "organizer-task-df58e.appspot.com",
+    messagingSenderId: "761819540181",
+    appId: "1:761819540181:web:c0b184a0ebc53111ab505c"
+  }
 };
 
 /*
